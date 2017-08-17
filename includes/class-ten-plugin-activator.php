e4ccb378ef6e7db13382d62f22270b716ff331ec
michael mason <michael@michaeljmason.com>
2017-08-17 15:02:57 -0700

    init repo

---
+++ includes/class-ten-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://mmason33.github.io
+ * @since      1.0.0
+ *
+ * @package    Ten_Plugin
+ * @subpackage Ten_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Ten_Plugin
+ * @subpackage Ten_Plugin/includes
+ * @author     Michael Mason <michael@michaeljmason.com>
+ */
+class Ten_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
