c8d8c6b2f92ddeba646db52fa48d1ab7964759a3
michael mason <michael@michaeljmason.com>
2017-08-18 13:42:23 -0700

    api call in php and js both working

--- public/partials/ten-plugin-public-display.php
+++ public/partials/ten-plugin-public-display.php
@@ -16,10 +16,19 @@
 <section id="Ten-Plugin" class="ten-plugin ten-plugin-widget" data-count="<?php echo get_option('ten_plugin_count'); ?>">
   <?php
 
+    $url      = 'http://thecatapi.com/api/images/get?api_key=MjE1MDY5&format=xml&results_per_page=' .get_option('ten_plugin_count');
+    $response = wp_remote_get($url);
+    $body     = wp_remote_retrieve_body($response);
+    $xml  = simplexml_load_string($body);
+    $code = $xml->data->images;
 
-    $response = wp_remote_get('http://thecatapi.com/api/images/get?api_key=MjE1MDY5&format=xml&results_per_page=' .get_option('ten_plugin_count'));
+    // var_dump($code);
 
+    foreach ($xml->data->images->image as $image ) {
+      // var_dump($image->url);
+      echo '<figure class="ten-plugin--image--wrap" style="background-image: url(' . $image->url . ');background-repeat:no-repeat;background-position:center center; background-size:cover;">
+      </figure>';
+    }
 
-  echo wp_remote_retrieve_body($response, 'url');
   ?>
 </section>
