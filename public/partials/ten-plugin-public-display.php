84e8554d9c5f3f2da9c76d9261e9b71bd1f9af43
michael mason <michael@michaeljmason.com>
2017-08-18 13:02:03 -0700

    php api calls?

--- public/partials/ten-plugin-public-display.php
+++ public/partials/ten-plugin-public-display.php
@@ -14,5 +14,12 @@
 ?>
 
 <section id="Ten-Plugin" class="ten-plugin ten-plugin-widget" data-count="<?php echo get_option('ten_plugin_count'); ?>">
+  <?php
 
+
+    $response = wp_remote_get('http://thecatapi.com/api/images/get?api_key=MjE1MDY5&format=xml&results_per_page=' .get_option('ten_plugin_count'));
+
+
+  echo wp_remote_retrieve_body($response, 'url');
+  ?>
 </section>
