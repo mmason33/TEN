76f4f51b8baf2163b4c0187f961eba65c564ee9c
michael mason <michael@michaeljmason.com>
2017-08-18 11:24:19 -0700

    javascript and file structure

--- public/js/ten-plugin-public.js
+++ public/js/ten-plugin-public.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 
-	// MjE1MDY5
+	// API Key - MjE1MDY5
 
 	/**
 	 * All of the code for your public-facing JavaScript source
@@ -31,5 +31,72 @@
 	 * Although scripts in the WordPress core, Plugins and Themes may be
 	 * practising this, we should strive to set a better example in our own work.
 	 */
+
+	 // Changes XML to JSON
+	 // Modified version from here: http://davidwalsh.name/convert-xml-json
+	 function xmlToJson(xml) {
+
+	   // Create the return object
+	   var obj = {};
+
+	   if (xml.nodeType == 1) { // element
+	     // do attributes
+	     if (xml.attributes.length > 0) {
+	     obj["@attributes"] = {};
+	       for (var j = 0; j < xml.attributes.length; j++) {
+	         var attribute = xml.attributes.item(j);
+	         obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+	       }
+	     }
+	   } else if (xml.nodeType == 3) { // text
+	     obj = xml.nodeValue;
+	   }
+
+	   // do children
+	   // If just one text node inside
+	   if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === 3) {
+	     obj = xml.childNodes[0].nodeValue;
+	   }
+	   else if (xml.hasChildNodes()) {
+	     for(var i = 0; i < xml.childNodes.length; i++) {
+	       var item = xml.childNodes.item(i);
+	       var nodeName = item.nodeName;
+	       if (typeof(obj[nodeName]) == "undefined") {
+	         obj[nodeName] = xmlToJson(item);
+	       } else {
+	         if (typeof(obj[nodeName].push) == "undefined") {
+	           var old = obj[nodeName];
+	           obj[nodeName] = [];
+	           obj[nodeName].push(old);
+	         }
+	         obj[nodeName].push(xmlToJson(item));
+	       }
+	     }
+	   }
+	   return obj;
+	 }
+
+	 $(document).ready( function () {
+		 var count = parseInt($('#Ten-Plugin').attr('data-count'));
+		 console.log(count);
+		 $.ajax({
+			 method: 'GET',
+			 url: 'http://thecatapi.com/api/images/get?api_key=MjE1MDY5&format=xml&results_per_page=3'
+		 }).done(function (response){
+			 var catData = xmlToJson(response);
+			 console.log(catData.response.data.images.image);
+
+			 for (var image in catData.response.data.images.image) {
+			 		console.log(catData.response.data.images.image[image].url);
+				 	$('#Ten-Plugin').append(
+						//background images to have all the images be the same size for formatting and styling purpose
+						'<figure class="ten-plugin--image--wrap" style="background: url(' + catData.response.data.images.image[image].url + ') no-repeat center center;background-size:cover;">' +
+							//'<img src="' + catData.response.data.images.image[image].url + '" alt="cat-image" class="ten-plugin--image">' +
+						'</figure>'
+					);
+			 }
+		 });
+	 });
+
 
 })( jQuery );
